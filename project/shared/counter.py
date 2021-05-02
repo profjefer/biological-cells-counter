b6191c8b09cb208acaa979f1c296c0b5798a8a12
pawellski <skibaapawell@gmail.com>
2021-05-02 17:28:39 +0200

    Added reduce_third_dimension function in counter

--- project/shared/counter.py
+++ project/shared/counter.py
@@ -5,3 +5,10 @@ def add_buffer(image):
         image[r].insert(0, 0)
         image[r].append(0)
     return image
+
+
+def reduce_third_dimension(image):
+    for i in range(len(image)):
+        for j in range(len(image[i])):
+            image[i][j] = image[i][j][0]
+    return image
