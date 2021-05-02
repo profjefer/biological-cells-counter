72492dfcfa856aa2b7a6dfe604f07fabd23cbdef
pawellski <skibaapawell@gmail.com>
2021-05-02 19:33:45 +0200

    Added check_neighbours and add_pixel_to_cells functions in counter

--- project/shared/counter.py
+++ project/shared/counter.py
@@ -1,3 +1,6 @@
+HALF_MOORE_COORDINATES = [[-1, -1], [-1, 0], [-1, 1], [0, -1]]
+
+
 def add_buffer(image):
     image.insert(0, [0] * len(image[0]))
     image.append([0] * len(image[0]))
@@ -22,3 +25,33 @@ def get_cell_index(pixel, cells):
         except ValueError:
             pass
     return -1
+
+
+def add_pixel_to_cells(pixel, cells, neighbours):
+    if len(neighbours) < 1:
+        cells.append([])
+        cells[-1].append(pixel)
+    elif len(neighbours) > 1:
+        cells[neighbours[0]].append(pixel)
+        for i in range(1, len(neighbours)):
+            cells[neighbours[0]].extend(cells[neighbours[i]])
+            cells.remove(cells[neighbours[i]])
+    else:
+        cells[neighbours[0]].append(pixel)
+
+    return cells
+
+
+def check_neighbours(submatrix, pixel, cells):
+    center_row, center_col = pixel
+    neighbours = []
+
+    for coord in HALF_MOORE_COORDINATES:
+        neighbour = (center_row + coord[0], center_col + coord[1])
+        if submatrix[coord[0] + 1][coord[1] + 1] == 255:
+            cell_index = get_cell_index(neighbour, cells)
+            if cell_index > -1:
+                neighbours.append(cell_index)
+    neighbours = list(set(neighbours))
+
+    return neighbours
